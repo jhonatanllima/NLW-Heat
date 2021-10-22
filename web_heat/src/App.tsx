1d3d0dd11dfeb12a388fcba0b7cf127b44128112
Jhonatan Lima <jhonatan.vinicius.lima@gmail.com>
2021-10-22 15:27:51 -0300

    :sparkles: (finalize web_heat)

---
+++ web_heat/src/App.tsx
@@ -0,0 +1,21 @@
+import { useContext } from "react";
+import styles from "./App.module.scss";
+
+import { LoginBox, MessageList, SendMessageForm } from "./components";
+import { AuthContext } from "./contexts/auth";
+
+export function App() {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <main
+      className={`${styles.contentWrapper} ${
+        !!user ? styles.contentSigned : ""
+      }`}
+    >
+      <MessageList />
+
+      {!!user ? <SendMessageForm /> : <LoginBox />}
+    </main>
+  );
+}
---
+++ web_heat/src/components/index.ts
@@ -0,0 +1,3 @@
+export { LoginBox } from "./LoginBox";
+export { MessageList } from "./MessageList";
+export { SendMessageForm } from "./SendMessageForm";
